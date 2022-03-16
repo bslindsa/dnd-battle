0c82a333b4e60edbe92f3bb4911d72879c44f1f5
Brad Lindsay <bslindsa@gmail.com>
2022-03-16 16:54:08 -0700

    fetch monster

--- client/src/pages/MonsterSelect/MonsterSelect.js
+++ client/src/pages/MonsterSelect/MonsterSelect.js
@@ -1,67 +1,62 @@
 import React, { useState } from 'react';
 // import { useQuery } from '@apollo/client';
-import { GET_MONSTER } from '../../utils/queries';
+// import { GET_MONSTER } from '../../utils/queries';
 // import Auth from '../../utils/auth'
 
 // 5e API graphQL URL: https://www.dnd5eapi.co/graphql
-const queryURL = 'https://www.dnd5eapi.co/graphql';
+// const queryURL = 'https://www.dnd5eapi.co/graphql';
 
 const MonsterSelect = () => {
     const [monIndex, setMonIndex] = useState('');
-
-    // const { loading, data } = useQuery(GET_MONSTER, {
-    //     variables: {
-    //         filter: {
-    //             index: monIndex
-    //         }
-    //     }
-    // });
 
     const handleMonSearchChange = (event) => {
         // eslint-disable-next-line
         const { name, value } = event.target;
         setMonIndex(value);
-        console.log(monIndex);
+    };
+
+    const fetchMonster = () => {
+
+        const monName = monIndex.toLowerCase().split(' ').join('-');
+        const queryMonsterURL = `https://www.dnd5eapi.co/api/monsters/${monName}`
+        fetch(queryMonsterURL)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                
+                console.log(data)
+
+                let name = data.name;
+                let str = data.strength;
+
+            })
+
+
     }
 
-    const handleFormSubmit = () => {
-        fetch(queryURL, {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify({
-                GET_MONSTER,
-                variables: {
-                    filter: {
-                        index: monIndex
-                    }
-                }
-            })
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            const monster = data?.monster
-            console.log(monster);
+    
 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        fetchMonster();
 
-        });
-
-        
     }
 
     return (
         <div>
-            <input type='text' value={monIndex} onChange={handleMonSearchChange} />
-            <button
-                className='custom-btn'
-                style={{cursor: 'pointer'}}
-                type='submit'
-                onSubmit={handleFormSubmit}
-            >
-                Search
-            </button>
+            <form onSubmit={handleFormSubmit}>
+                <label>Search Monster: </label>
+                <input type='text' value={monIndex} onChange={handleMonSearchChange} />
+                <button
+                    className='custom-btn'
+                    style={{ cursor: 'pointer' }}
+                    type='submit'
+                >
+                    Search
+                </button>
+            </form>
+
 
         </div>
 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
 
-// let userDB = new mongoose();
-// let fiveEDB = new mongoose();
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dndbattle', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-// fiveEDB.connect(`https://www.dnd5eapi.co/graphql`, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 module.exports = mongoose.connection;
